02c6b556bad6a0a9b00a4f7b331a83850079b6c9
Massa-Albani4 <massa.albani0213@gmail.com>
2024-09-12 17:14:38 +0300

    Add css styles

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn");
+const dropdown = document.getElementById("dropdown");
+const arrow_down = document.getElementById("arrow_down");
+const arrow_up = document.getElementById("arrow_up");
+
+btn.addEventListener("click", () => {
+  dropdown.style.display = "inline";
+});
